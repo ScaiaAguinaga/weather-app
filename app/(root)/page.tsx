3ac99024fe4a619ff3c677d829711f0c7f723b31
Isaac <Scaiaaguinaga@gmail.com>
2024-12-09 15:31:22 -0800

    Updated current weather data api call to accept user input

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,11 +5,16 @@ import { useState } from "react";
 const Page = () => {
   const [info, setInfo] = useState("");
   const [info2, setInfo2] = useState("");
+  const [cityName, setCityName] = useState("");
 
   const fetchData = async () => {
     // Fetch current data
     const response = await fetch("/api/currentData", {
-      method: "GET",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ city: cityName }),
     });
 
     const data = await response.json();
@@ -28,6 +33,14 @@ const Page = () => {
 
   return (
     <>
+      <input
+        type='text'
+        onChange={(e) => {
+          setCityName(e.target.value);
+        }}
+        className='border-2 border-gray-500'
+      />
+      <h1>{cityName}</h1>
       <button
         onClick={fetchData}
         className='bg-blue-500 rounded-full px-4 py-2 '
--- app/api/currentData/route.ts
+++ app/api/currentData/route.ts
@@ -1,21 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+
 /**
  * Fetches the current weather data for requested city using the WeatherAPI.
  * Returns a JSON response with the weather data or an error message if the API key is missing.
  */
 
-export async function GET() {
+export async function POST(req: NextRequest) {
   const APIKEY = process.env.WEATHERAPI_KEY;
+
   if (!APIKEY) {
-    return new Response(JSON.stringify({ error: "API key is missing" }), {
-      status: 500,
-    });
+    return NextResponse.json({ error: "API key is missing" }, { status: 500 });
+  }
+
+  const { city } = await req.json();
+
+  if (!city) {
+    return NextResponse.json({ error: "City is missing" }, { status: 400 });
   }
 
   const response = await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=${APIKEY}&q=Moreno_Valley&aqi=no`
+    `http://api.weatherapi.com/v1/current.json?key=${APIKEY}&q=${city}&aqi=no`
   );
 
   const data = await response.json();
 
-  return new Response(JSON.stringify(data));
+  return NextResponse.json(data);
 }
